c689e264077977b26626443db7cf3896c50ec377
VelixsCraftMC <muhammadilyas030906@gmail.com>
2025-05-09 19:04:48 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,50 @@
 const CACHE_NAME = 'acumalaka-v1.0.2-beta';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/config.json',
-  '/icon/icon.png',
-  '/ikon/home.png',
-  '/ikon/events.png',
-  '/ikon/info.png',
-  '/ikon/download.png',
-  '/bebas/bebas.png'
+'/',
+'/index.html',
+'/style.css',
+'/script.js',
+'/config.json',
+
+'/app/icon.png',
+
+'/app/banner.png',
+'/app/banner_event',
+'/app/banner_info.png',
+'/app/banner_download.png',
+
+'/app/home.png',
+'/app/event.png',
+'/app/info.png',
+'/app/download.png',
+'/app/bebas.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS_TO_CACHE))
-      .then(() => self.skipWaiting())
-  );
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(ASSETS_TO_CACHE))
+.then(() => self.skipWaiting())
+);
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
+event.waitUntil(
+caches.keys().then(cacheNames => {
+return Promise.all(
+cacheNames.map(cache => {
+if (cache !== CACHE_NAME) {
+return caches.delete(cache);
+}
+})
+);
+})
+);
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+event.respondWith(
+caches.match(event.request)
+.then(response => response || fetch(event.request))
+);
 });
